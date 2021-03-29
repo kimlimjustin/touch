a9bf44c394bb1d8532144a33850e66409f3793d1
kimlimjustin <kimlimjustin@gmail.com>
2021-03-29 16:31:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Touch
+`touch` command for windows. Create file(s) by typing command.
+
+## How to use:
+- Clone or fork this repository
+- Register the folder in `PATH` environment variable
+- Done :)
+
+## License
+MIT
---
+++ touch.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    if(argc == 1){
+        printf("Usage: touch [file name]");
+    }else{
+        int i;
+        char cmd[1024];
+        for(i = 1; i <argc ; i++){
+            if(i == 1){
+                strcat(cmd, "copy NULL > ");
+            }else{
+                strcat(cmd, " & copy NULL > ");
+            }
+            strcat(cmd, argv[i]);
+        }
+        system(cmd);
+    }
+}
