cfb39e9c969de59c01ed20e566119db6f153ec3a
kimlimjustin <kimlimjustin@gmail.com>
2021-06-03 13:14:08 +0700

    fix: the system cannot find the file specified message on new created file

--- touch.c
+++ touch.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[]){
     if(argc == 1){
         printf("Usage: touch [file name]");
     }else{
         int i;
-        char cmd[1024] = "";
+        FILE *fPtr;
         for(i = 1; i <argc ; i++){
-            if(i == 1){
-                strcat(cmd, "copy NULL > ");
-            }else{
-                strcat(cmd, " & copy NULL > ");
+            char *arg = strdup(argv[i]);
+
+            char path[1024] = "";
+            char *ptr = strtok(dirname(argv[i]), "/");
+            while(ptr != NULL){
+                if(strcmp(path, "")) {
+                    strcat(path, "\\\\");
+                }
+                strcat(path, ptr);
+                char cmd[1024] = "mkdir ";
+                strcat(cmd, path);
+
+                struct stat sb;
+                if(!(stat(path, &sb) == 0 && S_ISDIR(sb.st_mode))){
+                    system(cmd);
+                }
+                ptr = strtok(NULL, "/");
             }
-            strcat(cmd, argv[i]);
+            fPtr = fopen(arg, "w");
+            fputs("", fPtr);
+            fclose(fPtr);
         }
-        system(cmd);
     }
 }
